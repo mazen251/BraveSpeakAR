45450216346f657b2ff20bd06a4051894e6e5e59
Mazen <mazen.walid251@gmail.com>
2024-06-12 12:17:19 +0300

    Added 3 New SideBar Actions

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,12 @@
 import Dashboard from "views/Dashboard.js";
 import Icons from "views/Icons.js";
 import TableList from "views/TableList.js";
+import WeightDistribution from "views/WeightDistribution.js";
 
 var routes = [
   {
     path: "/Icons",
     name: "Homepage",
-    rtlName: "الرموز",
     icon: "tim-icons icon-bank",
     component: <Icons />,
     layout: "/admin",
@@ -14,8 +14,28 @@ var routes = [
   {
     path: "/tables",
     name: "Patients List",
-    rtlName: "قائمة الجدول",
     icon: "tim-icons icon-single-02",
+    component: <TableList />,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "weight distribution",
+    icon: "tim-icons icon-chart-bar-32",
+    component: <TableList />,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "Survey",
+    icon: "tim-icons icon-notes",
+    component: <TableList />,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "Video Upload",
+    icon: "tim-icons icon-video-66",
     component: <TableList />,
     layout: "/admin",
   },
